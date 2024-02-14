72e60d4d249698548b25593e71d8e51d8f16ac2e
j-a-valentine <j.a.valentine@wustl.edu>
2024-02-13 20:29:41 -0600

    react start

---
+++ frontend/src/components/Auth.js
@@ -0,0 +1,20 @@
+import '../styles/Auth.css'
+import SignUp from './SignUp'
+
+
+let signUpUser = () => {
+    console.log("Signing Up")
+    
+}
+
+
+function Auth() {
+    return (
+        <div>
+            <SignUp />
+        </div>
+    )
+
+}
+
+export default Auth
---
+++ frontend/src/components/SignUp.js
@@ -0,0 +1,12 @@
+
+function SignUp() {
+    return (
+
+        <form>
+            <input type="text" name="email" id="email" placeholder="student@wustl.edu"></input>
+        </form>
+
+    )
+}
+
+export default SignUp
