f7b13d3a29c92eb7413347220d906a57cbf8b212
j-a-valentine <j.a.valentine@wustl.edu>
2024-02-19 12:09:16 -0600

    added mui

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,12 +1,14 @@
-
+import Button from '@mui/material/Button';
 function SignUp() {
     return (
 
         <form>
             <input type="text" name="email" id="email" placeholder="student@wustl.edu"></input>
+            <Button variant="contained">Hello world</Button>;
         </form>
 
     )
 }
 
 export default SignUp
+
