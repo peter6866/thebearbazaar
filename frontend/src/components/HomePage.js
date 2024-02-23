4ca93ad0f08ecafd1170319ea4cd7ef2807ff7d7
j-a-valentine <j.a.valentine@wustl.edu>
2024-02-23 18:17:37 -0500

    no login button

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,5 +1,5 @@
 // TODO: Create a home page for the Bear Bazaar
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import Transact from "./Transact";
@@ -8,25 +8,20 @@ function HomePage() {
   const { isLoggedIn, logout } = useAuth();
   let navigate = useNavigate();
 
-  function handleLoginClick() {
-    navigate("/auth");
-  }
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/auth");
+    }
+  }, [isLoggedIn, navigate]);
 
   return (
     // FIXME: temporary style
     <div className="auth-container-outer">
-      {isLoggedIn ? (
-        <div>
-          <h1>Welcome back!</h1>
-          <button onClick={logout}>Logout</button>
-          <Transact />
-        </div>
-      ) : (
-        <div>
-          <h1>Please log in.</h1>
-          <button onClick={handleLoginClick}>Login</button>
-        </div>
-      )}
+      <div>
+        <h1>Welcome back!</h1>
+        <button onClick={logout}>Logout</button>
+        <Transact />
+      </div>
     </div>
   );
 }
