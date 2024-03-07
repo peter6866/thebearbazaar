3a8687dd3b71528fd4db1551f4a36d33732b3760
Sparmon7 <armon.s@wustl.edu>
2024-03-06 21:42:07 -0600

    adjusted to keep aws stuff

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -5,6 +5,7 @@ const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+const aws = require("@aws-sdk/client-ses");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -12,15 +13,34 @@ const signToken = (id) => {
   });
 };
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.sendgrid.net",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "apikey",
-    pass: process.env.SENDGRID_API_KEY,
+// AWS SES Credentials
+const ses = new aws.SES({
+  apiVersion: "2012-10-17",
+  region: "us-east-2",
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
   },
 });
+
+let transporter;
+// use ses for production
+if (process.env.NODE_DEV_ENV === "production") {
+  transporter = nodemailer.createTransport({
+    SES: { ses, aws },
+  });
+  // use sendgrid for development
+} else {
+  transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "apikey",
+      pass: process.env.SENDGRID_API_KEY,
+    },
+  });
+}
 
 // helper function to send verification email
 const sendVerificationEmail = (email, verificationCode) => {
