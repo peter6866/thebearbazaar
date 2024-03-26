a88265baa285c13c711fd08af744dc943f92ac4b
j-a-valentine <j.a.valentine@wustl.edu>
2024-03-25 22:25:22 -0500

    potential mobile scroll fix?

--- frontend/src/components/PriceHistory.js
+++ frontend/src/components/PriceHistory.js
@@ -8,27 +8,29 @@ import {
 IgrCategoryChartModule.register();
 
 const PriceHistory = ({ history }) => {
-  return (history.length > 0 &&
-    <div>
-    <Divider style={{ marginTop: "2rem", marginBottom: "3rem" }}></Divider>
-      <h4>Meal Point Price History</h4>
-    <Box>
-      <IgrCategoryChart
-        width="100%"
-        height="300px"
-        dataSource={history}
-        xAxisTitle="Date"
-        yAxisTitle="Price of 500 meal points"
-        xAxisLabelLocation="OutsideBottom"
-        yAxisLabelLocation="OutsideLeft"
-        xAxisOverlap="auto"
-        yAxisMinimumValue={0}
-        chartType="line"
-        isHorizontalZoomEnabled="false"
-        isVerticalZoomEnabled="false"
-      />
-    </Box>
-    </div>
+  return (
+    history.length > 0 && (
+      <div>
+        <Divider style={{ marginTop: "2rem", marginBottom: "3rem" }}></Divider>
+        <h4>Meal Point Price History</h4>
+        <Box className="disable-touch">
+          <IgrCategoryChart
+            width="100%"
+            height="300px"
+            dataSource={history}
+            xAxisTitle="Date"
+            yAxisTitle="Price of 500 meal points"
+            xAxisLabelLocation="OutsideBottom"
+            yAxisLabelLocation="OutsideLeft"
+            xAxisOverlap="auto"
+            yAxisMinimumValue={0}
+            chartType="line"
+            isHorizontalZoomEnabled="false"
+            isVerticalZoomEnabled="false"
+          />
+        </Box>
+      </div>
+    )
   );
 };
 
