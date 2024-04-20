17d5cde709cc82156fff48b02d97d7256205ee14
Jiayu Huang <hjiayu@wustl.edu>
2024-04-20 18:01:40 -0500

    update eslint action

--- backend/.eslint.config.js
+++ backend/.eslint.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  extends: ["airbnb", "prettier", "plugin:node/recommended"],
-  plugins: ["prettier"],
+  extends: ["airbnb", "plugin:node/recommended"],
   rules: {
-    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "comma-dangle": "off",
     "spaced-comment": "off",
     "no-console": "warn",
     "consistent-return": "off",
@@ -10,9 +9,11 @@ module.exports = {
     "object-shorthand": "off",
     "no-process-exit": "off",
     "no-return-await": "off",
+    "no-param-reassign": "off",
     "no-underscore-dangle": "off",
     "class-methods-use-this": "off",
     "prefer-destructuring": ["error", { object: true, array: false }],
     "no-unused-vars": ["error", { argsIgnorePattern: "req|res|next|val" }],
+    "import/newline-after-import": "off",
   },
 };
